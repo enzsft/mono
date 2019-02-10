78d1811c9ce1b08217625a0402b776c352854181
leecheneler <lacheneler@gmail.com>
2019-02-10 23:20:43 +0000

    updated @enzsft/cli

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,16 @@
 #! /usr/bin/env node
 
 import { createCli } from "@enzsft/cli";
+import { createAddCommand } from "./commands/add";
 import { createRunCommand } from "./commands/run";
 import { getPackages } from "./packages";
 
 getPackages(process.cwd()).then(packages => {
   const cli = createCli({
-    commands: [createRunCommand(packages)],
+    commands: [createAddCommand(packages), createRunCommand(packages)],
     description: "Manage JavaScript mono repos with ease. 😲",
     name: "mono",
+    version: require("./package.json").version,
   });
 
   return cli.start(process.argv).catch(() => process.exit(1));
